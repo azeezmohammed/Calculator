54dc144b1c602558fd5b7ae4c7f96eb4dd730df2
ADMIN <ADMIN@192.168.0.17>
2017-05-31 22:11:58 -0700

    Complete Calculator code Updated on 1/6/2017...

--- src/com/onlinecalculator/calculator/calctypes/ScientificCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/ScientificCalc.java
@@ -1,23 +1,70 @@
 package com.onlinecalculator.calculator.calctypes;
 
-import java.util.Scanner;
-
 import java.lang.Math;
 
 public class ScientificCalc extends BasicCalc{
+	
 	public int menu(){
   	
+		System.out.println("------***------");
+    	System.out.println("1: Addition");
+    	System.out.println("2: Subtraction");
+    	System.out.println("3: Multiplication");
+    	System.out.println("4: Division");
+    	System.out.println("5: Power");
+    	System.out.println("6: SquareRoot");
+    	System.out.println("7: Sin");
+    	System.out.println("8: Exit");
+    	System.out.println("------***------");
+    	System.out.println("Enter your choice");
+    	ch=s.nextInt();
+    	return ch;
 	}
 
   public void userInputs()
   {
   	
+	  
+	  System.out.println("Enter first number");
+	  a=s.nextInt();
+	  if(ch!=6 && ch!=7){
+  	System.out.println("Enter second number");
+  	b=s.nextInt();
+	  }
+  	
+	
   }
 
   public void performOperation(){
-  		switch(choice)
+	  
+  		switch(ch)
   		{
   		
+  		case 1:
+  		case 2:
+  		case 3:
+  		case 4:
+  			super.performOperation();
+  			break;
+  			
+  		case 5:
+  			System.out.println("Result: "+pwrof());
+    		break;
+    		
+  		case 6:
+  			System.out.println("Result: "+sqrtof());
+    		break;
+    		
+  		case 7:
+  			System.out.println("Result: "+sineof());
+    		break;
+    		
+  		case 8:
+  			System.out.println("Thank You");
+    		break;
+    		
+  		default:
+  			System.out.println("Invalid Selection");
   	}
       
   }
@@ -25,16 +72,19 @@ public class ScientificCalc extends BasicCalc{
 	double pwrof()
 	{
 		
+		return Math.pow(a,b);
 	}
 	
 	double sqrtof()
 	{
 		
+		return Math.sqrt(a);
 	}
 
 	double sineof()
 	{
-		
+		double rad=Math.toRadians(a);
+		return Math.sin(rad);
 	}
 }
 
--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -5,33 +5,63 @@ import java.util.Scanner;
 import com.onlinecalculator.calculator.calctypes.*;
 
 public class MainPgm {
-public static void main(String args[]){
 	
-	BasicCalc bc=new BasicCalc();
-	bc.userInputs();
-	bc.menu();
-	bc.performOperation();
+	static Scanner s=new Scanner(System.in);
+	
+public static void main(String args[]){
 	
         char type;
         type=getType();
         if(type=='a')
         {
         	
+        	BasicCalc bc=new BasicCalc();
+        	bc.menu();
+        	bc.userInputs();
+        	bc.performOperation();
         }
+        
         if(type=='b')
         {
        		
+        	ScientificCalc sc=new ScientificCalc();
+        	sc.menu();
+        	sc.userInputs();
+        	sc.performOperation();
+        	
        	}
         	
         	if(type=='c')
         	{
         		
+        		StatisticCalc st=new StatisticCalc();
+        		st.menu();
+            	st.userInputs();
+            	st.performOperation();
         	}
  
     }
         
     static char getType()
     {
+    	
+    	System.out.println("------***------");
+    	System.out.println("1: BasicCalc");
+    	System.out.println("2: ScientificCalc");
+    	System.out.println("3: StatisticCalc");
+    	System.out.println("------***------");
+    	System.out.println("Enter your choice");
+    	int ch=s.nextInt();
+    	
+    	if(ch==1)
     	return 'a';
+    	else if(ch==2)
+    		return 'b';
+    	else if(ch==3)
+    		return 'c';
+    	else
+    		System.out.println("Invalid Selection");
+    	return 'd';
+    
     }
 }
